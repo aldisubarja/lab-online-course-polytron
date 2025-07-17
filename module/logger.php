ae5e4ce2856900ae86922619e44d3c9b32b29d6e
mirandachristanto <mirandachristanto@gmail.com>
2025-07-17 14:29:09 +0700

    Add logger to login, register pages

---
+++ module/logger.php
@@ -0,0 +1,14 @@
+<?php
+function writeLogToFile($message, $logDir = './logs/') {
+    // Ensure log directory exists
+    if (!file_exists($logDir)) {
+        mkdir($logDir, 0775, true);
+    }
+
+    $date = date('Y-m-d');
+    $timestamp = date('Y-m-d H:i:s');
+    $fileName = $logDir . "Log-$date.txt";
+    $logEntry = "[$timestamp] $message" . PHP_EOL;
+
+    file_put_contents($fileName, $logEntry, FILE_APPEND);
+}
